ed7689cec8b5f665cff59f38fa4a699afe7a8da8
DarkAriesX <skrzys2103@gmail.com>
2020-12-02 22:10:06 +0100

    formating

--- Asteroids.hpp
+++ Asteroids.hpp
@@ -10,3 +10,9 @@ int side();
 sf::Vector2f generatePosition(const int& side);
 std::string generateScoreText(const int& score);
 void fireShot(sf::CircleShape& bullet, sf::CircleShape& player, sf::Sound*& shot);
+void control(sf::RenderWindow& window, sf::CircleShape& player, sf::Event& event);
+void fire(sf::RenderWindow& window, sf::CircleShape& player, sf::Event& event, 
+            sf::CircleShape& bullet);
+void asteroidMove(sf::RenderWindow& window, sf::CircleShape& player, 
+                    std::array<sf::CircleShape, 4>& asteroid, sf::CircleShape& bullet, 
+                    sf::Text& scoreText);
--- main.cpp
+++ main.cpp
@@ -3,15 +3,10 @@
 #include <array>
 #include <cmath>
 #include <iostream>
+#include <X11/Xlib.h>
 
-
-void control(sf::RenderWindow& window, sf::CircleShape& player, sf::Event& event);
-void fire(sf::RenderWindow& window, sf::CircleShape& player, sf::Event& event, 
-            sf::CircleShape& bullet);
-void asteroidMove(sf::RenderWindow& window, sf::CircleShape& player, 
-                    std::array<sf::CircleShape, 4>& asteroid, sf::CircleShape& bullet, 
-                    sf::Text& scoreText);
 int main() {
+    XInitThreads();
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
     sf::RenderWindow window(sf::VideoMode(800, 600), "Asteroids", sf::Style::Default, settings);
@@ -96,111 +91,4 @@ int main() {
     delete fireThread;
     delete asteroidMoveThread;
     return 0;
-}
-void control(sf::RenderWindow& window, sf::CircleShape& player, sf::Event& event) {
-    while(window.isOpen()) {
-        float accelleration = 5;
-        while(window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-            if (event.type == sf::Event::KeyPressed) {
-                switch(event.key.code) {
-                    case (sf::Keyboard::Left):
-                        player.rotate(-accelleration);
-                        accelleration*=2.5;
-                        break;
-                    case (sf::Keyboard::Right):
-                        player.rotate(accelleration);
-                        accelleration*=2.5;
-                        break;
-                }
-            }
-        }
-            if ((sf::Joystick::isButtonPressed(0, 4)) && (sf::Joystick::isButtonPressed(0, 5))) {
-                player.rotate(0);
-            } else if (sf::Joystick::isButtonPressed(0, 4)) {
-                player.rotate(-0.001);
-            } else if (sf::Joystick::isButtonPressed(0, 5)) {
-                player.rotate(0.001);
-            } else if (sf::Joystick::isButtonPressed(0, 7)) {
-                window.close();
-            }
-    }
-}
-void fire(sf::RenderWindow& window, sf::CircleShape& player, sf::Event& event, 
-            sf::CircleShape& bullet) {
-    sf::SoundBuffer* buffer = new sf::SoundBuffer;;
-    buffer->loadFromFile("sounds/shot.ogg");
-    sf::Sound* shot = new sf::Sound;
-    shot->setBuffer(*buffer);
-    while(window.isOpen()) {
-        if (sf::Joystick::isButtonPressed(0, 0)) {
-            fireShot(bullet, player, shot);
-        }
-        while(window.pollEvent(event)) {
-            if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::Space) {
-                    fireShot(bullet, player, shot);
-                }
-            }
-        }
-    }  
-}
-void asteroidMove(sf::RenderWindow& window, sf::CircleShape& player, 
-                    std::array<sf::CircleShape, 4>& asteroid, sf::CircleShape& bullet, 
-                    sf::Text& scoreText) {
-    sf::FloatRect bulletBox;
-    sf::FloatRect asteroidBox;
-    sf::FloatRect playerBox;
-    int score = 0;
-    sf::Clock clock[4];
-    sf::SoundBuffer* buffer = new sf::SoundBuffer;;
-    buffer->loadFromFile("sounds/boom.ogg");
-    sf::Sound* boom = new sf::Sound;
-    boom->setBuffer(*buffer);
-    int asteroidClockIterator = 0;
-    while(window.isOpen()) {
-        float a{};
-        float b{};
-        for (auto& el : asteroid) {
-            if (clock[asteroidClockIterator].getElapsedTime() >= sf::seconds(2)) {
-                if ((el.getPosition().x >= 400) && (el.getPosition().y >= 300)) {
-                    a = (el.getPosition().x - player.getPosition().x) * -1;
-                    b = (el.getPosition().y - player.getPosition().y) * -1;
-                } else if ((el.getPosition().x >= 400) && (el.getPosition().y < 300)) {
-                    a = (el.getPosition().x - player.getPosition().x) * -1;
-                    b = (el.getPosition().y - player.getPosition().y) * -1;
-                } else if ((el.getPosition().x < 400) && (el.getPosition().y < 300)) {
-                    a = (el.getPosition().x - player.getPosition().x) * -1;
-                    b = (el.getPosition().y - player.getPosition().y) * -1;
-                } else {
-                    a = (el.getPosition().x - player.getPosition().x) * -1;
-                    b = (el.getPosition().y - player.getPosition().y) * -1;
-                }
-                a = a/5000;
-                b = b/5000;
-                el.move(a, b);
-            }
-            bulletBox = bullet.getGlobalBounds();
-            asteroidBox = el.getGlobalBounds();
-            playerBox = player.getGlobalBounds();
-            if (playerBox.intersects(asteroidBox)) {
-                el.setPosition(player.getPosition());
-                std::this_thread::sleep_for(std::chrono::seconds(3600));
-            } else if(bulletBox.intersects(asteroidBox)) {
-                boom->play();
-                clock[asteroidClockIterator].restart();
-                el.setPosition(generatePosition(side()));
-                bullet.setPosition(1000, 1000);
-                score++;
-                scoreText.setString(generateScoreText(score));
-            }
-            asteroidClockIterator++;
-            if (asteroidClockIterator > 3) {
-                asteroidClockIterator = 0;
-            }
-        }
-        std::this_thread::sleep_for(std::chrono::microseconds(400 - score));
-    }
 }
